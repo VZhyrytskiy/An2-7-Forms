2fa0418e737acb53787b42b5c0670e0d8ab42a94
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-05-29 12:23:34 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { SignupFormComponent } from './template-driven-forms/signup-form'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
