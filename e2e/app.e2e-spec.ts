9f02efa4e92bfe9fd004c57b392c9006b3f52d5a
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-11-16 14:36:28 +0200

    angular was updated to v5

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularFormsPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('angular-forms App', function() {
-  let page: AngularFormsPage;
+describe('an App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularFormsPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
