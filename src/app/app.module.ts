9ec116187b67005f930c30647b9ffe78edbd784d
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-06-13 13:29:38 +0300

    update project to Angular v6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { SignupFormComponent } from './template-driven-forms/signup-form'
+import { SignupFormComponent } from './template-driven-forms/signup-form/signup-form.component';
+import { SignupReactiveFormComponent } from './reactive-forms/signup-reactive-form/signup-reactive-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupFormComponent
+    SignupFormComponent,
+    SignupReactiveFormComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-reactive-form',
+  templateUrl: './signup-reactive-form.component.html',
+  styleUrls: ['./signup-reactive-form.component.css']
+})
+export class SignupReactiveFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
